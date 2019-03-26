bb5ac9bca6882d64a746806d572322a815c5447b
XojeHome <dawxoje@gmail.com>
2019-03-26 14:34:12 +0100

    Intentando Solucion a base de funciones

--- Con Debora/JavaScript/3.3.Solucion/js/main.js
+++ Con Debora/JavaScript/3.3.Solucion/js/main.js
@@ -4,9 +4,33 @@ window.onload=function(){
 }
 
 function creaPagina(){
-
+    insertElementText("h1","Gaspatxo");
+    insertElementImagen("./img/gaspatxo.jpg");
+    insertElement("hr");
+    insertElementText("h2","Ingredients");
+    insertElement("ul");
+    insertSubElementText("li","ul");
 }
 
 function creaEstilos(){
     
+}
+
+function insertElement(elemento) {
+    var etiqueta=document.createElement(elemento);
+    document.body.appendChild(etiqueta);
+}
+function insertSubElement(elemento,elementoPadre) {    
+    
+    insertElementText(elemento,texto);
+}
+function insertElementText(elemento,texto){
+    insertElement(elemento);
+    var contenido=document.createTextNode(texto);
+    etiqueta.appendChild(contenido);
+}
+function insertElementImagen(path) {
+    insertElement("img");
+    var ruta=path;
+    etiqueta.setAttribute("src",ruta);
 }
