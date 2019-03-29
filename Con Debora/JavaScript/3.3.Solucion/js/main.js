e90b310f31547f354d493ffd2f3145273b094e24
XojeHome <dawxoje@gmail.com>
2019-03-29 18:48:58 +0100

    Fallo con la lista

--- Con Debora/JavaScript/3.3.Solucion/js/main.js
+++ Con Debora/JavaScript/3.3.Solucion/js/main.js
@@ -4,18 +4,47 @@ window.onload=function(){
 }
 
 function creaPagina(){
-    insertElementText("h1","Gaspatxo");
+    var titulo=document.createElement("h1");
+    var textoTitulo=document.createTextNode("Gaspatxo");
+    document.body.appendChild(titulo);
+    titulo.appendChild(textoTitulo);
+    
+    var marco=document.createElement("img");
+    var ruta="./img/gaspatxo.jpg";
+    document.body.appendChild(marco);
+    marco.setAttribute("src",ruta);
+
+    var linea=document.createElement("hr");
+    document.body.appendChild(linea);
+
+    var subTitulo=document.createElement("h2");
+    var textoSubTitulo=document.createTextNode("Ingredients");
+    document.body.appendChild(subTitulo);
+    subTitulo.appendChild(textoSubTitulo);
+
+    var listaDesordenada=document.createElement("ul");
+    document.body.appendChild(listaDesordenada);
+
+    var itemLista=document.createElement("li");
+    var contenidoItemLista=document.createTextNode("Tomaca");
+    itemLista.appendChild(contenidoItemLista);
+    listaDesordenada.appendChild(itemLista);
+
+    contenidoItemLista=document.createTextNode("Pebrot verd");
+    itemLista.appendChild(contenidoItemLista);
+    listaDesordenada.appendChild(itemLista);
+    /*insertElementText("h1","Gaspatxo");
     insertElementImagen("./img/gaspatxo.jpg");
     insertElement("hr");
     insertElementText("h2","Ingredients");
     insertElement("ul");
-    insertSubElementText("li","ul");
+    insertSubElementText("li","ul");*/
 }
 
 function creaEstilos(){
     
 }
-
+/*
 function insertElement(elemento) {
     var etiqueta=document.createElement(elemento);
     document.body.appendChild(etiqueta);
@@ -32,4 +61,4 @@ function insertElementImagen(path) {
     insertElement("img");
     var ruta=path;
     etiqueta.setAttribute("src",ruta);
-}
+}*/
